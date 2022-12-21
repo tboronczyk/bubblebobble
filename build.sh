794e380023098a5c902624109127b5517d131f4c
Timothy Boronczyk <tboronczyk@gmail.com>
2022-12-21 11:53:28 -0500

    shift letters 1px

--- build.sh
+++ build.sh
@@ -11,6 +11,12 @@ cp bb.nes ../$ROM
 cd ..
 
 echo "Updating CHR..."
-# ...
+dd if=chr/alphabet1.bin of="$ROM" conv=notrunc bs=1 seek=$((0x20010))
+dd if=chr/alphabet2.bin of="$ROM" conv=notrunc bs=1 seek=$((0x22010))
+dd if=chr/alphabet2.bin of="$ROM" conv=notrunc bs=1 seek=$((0x23010))
+dd if=chr/alphabet2.bin of="$ROM" conv=notrunc bs=1 seek=$((0x24010))
+dd if=chr/alphabet2.bin of="$ROM" conv=notrunc bs=1 seek=$((0x25010))
+dd if=chr/alphabet2.bin of="$ROM" conv=notrunc bs=1 seek=$((0x27010))
+dd if=chr/underline.bin of="$ROM" conv=notrunc bs=1 seek=$((0x27e90))
 
 echo "Done"
