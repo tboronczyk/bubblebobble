9abcf4a789fe68f660af96f560a084daf37d4047
Timothy Boronczyk <tboronczyk@gmail.com>
2022-12-24 19:28:01 -0500

    extend/1up

--- build.sh
+++ build.sh
@@ -21,5 +21,13 @@ dd if=chr/underline.bin of="$ROM" conv=notrunc bs=1 seek=$((0x27e90))
 dd if=chr/alphabet2-w.bin of="$ROM" conv=notrunc bs=1 seek=$((0x27ea0))
 dd if=chr/alphabet2-y.bin of="$ROM" conv=notrunc bs=1 seek=$((0x27eb0))
 dd if=chr/happyend.bin of="$ROM" conv=notrunc bs=1 seek=$((0x27470))
+dd if=chr/extend.bin of="$ROM" conv=notrunc bs=1 seek=$((0x22290))
+dd if=chr/extend.bin of="$ROM" conv=notrunc bs=1 seek=$((0x23290))
+dd if=chr/extend.bin of="$ROM" conv=notrunc bs=1 seek=$((0x24290))
+dd if=chr/extend.bin of="$ROM" conv=notrunc bs=1 seek=$((0x25290))
+dd if=chr/plus.bin of="$ROM" conv=notrunc bs=1 seek=$((0x22ff0))
+dd if=chr/plus.bin of="$ROM" conv=notrunc bs=1 seek=$((0x23ff0))
+dd if=chr/plus.bin of="$ROM" conv=notrunc bs=1 seek=$((0x24ff0))
+dd if=chr/plus.bin of="$ROM" conv=notrunc bs=1 seek=$((0x25ff0))
 
 echo "Done"
